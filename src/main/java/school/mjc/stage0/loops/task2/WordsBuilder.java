0b2140980e0056d185e15f15be801c10e4fd82b1
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-22 21:53:21 +0600

    Stage 0 - Module 4.2 - 2 - 2

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.loops.task2;
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
         String phrase = new String(chars);
-        System.out.println(phrase);
+        System.out.print(phrase);
     }
 }
